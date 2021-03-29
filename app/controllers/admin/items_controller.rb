a227a920bfa67c3aa0734a1dc030b9327e5c1b83
YutaYamaki <8makicam@gmail.com>
2021-03-29 02:52:28 +0000

    [fix]商品情報の更新

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
+    @items = Item.all
   end
 
   def new
@@ -8,24 +9,40 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
-  
+
   def create
-    Item.create(item_params)
-    redirect_to "/index"
+    @item = Item.new(item_params)
+    item = @item
+    if item.save
+     flash[:notice] = "item was successfully created."
+    redirect_to admin_item_path(item.id)
+    else
+      render :new
+    end
   end
-  
+
   def update
+    @item = Item.find(params[:id])
+    item = @item
+    if item.update(item_params)
+      flash[:notice] = "Profile was successfully updated."
+    redirect_to admin_item_path(item.id)
+    else
+      render :edit
+    end
   end
-  
+
   private
   def item_params
     params.require(:item).permit(:name, :introduction, :price, :genre_id, :is_active, :image)
   end
-  
+
 end
 
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9c050b8fea310fcd87f9d9a051484b147347f926ed91fb2fd31bac590460194597759c91d65e3f8e1991dee99849bae2ee7d49545c63bc07f602ddf96d9f3e63'
