2cff8ea93754c74686cb8a805019711de987628d
YutaYamaki <8makicam@gmail.com>
2021-04-13 08:01:09 +0000

    [fix]ジャンル&販売ステータスの表示

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,26 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
   
   def update
+    @customer = Customer.find(params[:id])
+    customer = @customer
+    if customer.update(customer_params)
+      flash[:notice] = "Customer was successfully updated."
+    redirect_to admin_customer_path
+    else
+    render :edit
+    end
   end
+  
+  private 
 end
